80a5edd3c92be5990ef825cd7e40ffaac886eabd
hemanthk04 <hkapalavai@gmail.com>
2025-05-27 21:05:41 +0530

    Updated Navbar with Github link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import GameBoard from "./GameBoard";
+import { GithubLogo } from "@phosphor-icons/react";
 
 function App() {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center space-y-8">
-      {/* Heading */}
-      <h1 className="font-jersey text-white text-4xl sm:text-5xl">
-        Snake Game
-      </h1>
+      <navbar className="w-full flex items-center justify-between px-12 py-4 bg-black">
+        <a
+          href="https://github.com/hemanthk04/snakegame"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-300"
+        >
+          <GithubLogo size={32} weight="fill" />
+        </a>
+        {/* Heading Text */}
+        <h1 className="font-jersey text-white text-4xl sm:text-5xl">
+          Snake Game
+        </h1>
+        <div className="w-[28px]">hello</div>
+      </navbar>
 
       {/* Game Board */}
       <GameBoard />
